0250c83296fe17c61596da82e423dcd172bc7848
KaMaDi <artkamadi@gmail.com>
2024-07-07 17:56:17 +0300

    Initial Commit

---
+++ iVictory/iVictoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  iVictoryApp.swift
+//  iVictory
+//
+//  Created by Jedi on 07.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct iVictoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
